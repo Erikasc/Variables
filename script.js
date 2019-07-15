0647247eb99b820bd6338c4686ba762d4011d3fd
Erikas Ciekanauskas <eciekanauskas@yahoo.com>
2019-07-15 13:58:30 +0300

    JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handlerUpdate() {
+   const suffix = this.dataset.sizing || '';
+   document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+}
+
+
+inputs.forEach(input => input.addEventListener('change', handlerUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handlerUpdate));
+
